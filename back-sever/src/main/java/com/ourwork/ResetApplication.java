629132dcf7e19d52c797bb1cacc8efce0100dc31
Keviiiiin <51362922+Keviiiiin@users.noreply.github.com>
2020-04-14 19:31:17 +0800

    Add files via upload
    增加了程序入口

--- back-sever/src/main/java/com/ourwork/ResetApplication.java
+++ back-sever/src/main/java/com/ourwork/ResetApplication.java
@@ -2,16 +2,14 @@ package com.ourwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Component
 @SpringBootApplication
 public class ResetApplication {
-
-
     public static void main(String[] args) {
-
         SpringApplication.run(ResetApplication.class, args);
     }
 }
