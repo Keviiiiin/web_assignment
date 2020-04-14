60c38a2fa7c10671d938b8b7172669e2fe9ad008
huxike <2212253497@qq.com>
2020-04-14 18:21:20 +0800

    forth commit

--- back-sever/src/main/java/com/ourwork/controller/controller.java
+++ back-sever/src/main/java/com/ourwork/controller/controller.java
@@ -13,6 +13,41 @@ import java.util.Optional;
 @RestController
 @RequestMapping( "/api/schedule" )
 public class controller {
+    @Autowired
+    public service taskService;
 
+    @GetMapping(produces = "application/json")
+    public List<model> list(){
+        return  taskService.getAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<model> find(@PathVariable Long id) {
+        return ResponseEntity.of(taskService.find(id));
+    }
 
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<model> create(@RequestBody model task) {
+        taskService.saveTask(task);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/tasks/{id}")
+                .buildAndExpand(task.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<model> update(@PathVariable Long id, @RequestBody model task) {
+        Optional<model> updatedTask = taskService.update(new model(id, task.getIncluding()));
+        return ResponseEntity.of(updatedTask);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        Optional<model> deletedTask = taskService.delete(id);
+        if (deletedTask.isPresent()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
+
