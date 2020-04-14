e5eecd4bc2e41ee925f670aa059c981a1f2c16d9
huxike <2212253497@qq.com>
2020-04-14 16:56:30 +0800

    second commit

--- back-sever/src/main/java/com/ourwork/ResetApplication.java
+++ back-sever/src/main/java/com/ourwork/ResetApplication.java
@@ -1,4 +1,9 @@
 package com.ourwork;
 
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
 public class ResetApplication {
 }
--- back-sever/src/main/java/com/ourwork/model/model.java
+++ back-sever/src/main/java/com/ourwork/model/model.java
@@ -1,4 +1,50 @@
 package com.ourwork.model;
 
+
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
 public class model {
+   private long id;
+   private String including;
+   private LocalDateTime uTime;
+
+   public model(){
+
+   }
+
+   public model(Long id, String including){
+      this.id = id;
+      this.including=including;
+   }
+
+   public String getIncluding() {
+      return including;
+   }
+
+   public void setIncluding(String including) {
+      this.including = including;
+   }
+
+   public LocalDateTime getuTime() {
+      return uTime;
+   }
+
+   public void setuTime(LocalDateTime uTime) {
+      this.uTime = uTime;
+   }
+
+   public long getId() {
+      return id;
+   }
+
+   public void setId(long id) {
+      this.id = id;
+   }
+
+
 }
+
